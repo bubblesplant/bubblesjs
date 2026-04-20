3f728ce77a1929b76e6c8ac4dbcd0f662a8ebb89
malei <435012357@qq.com>
2026-04-20 18:38:27 +0800

    feat: 初始化monorepo项目结构并添加基础配置

---
+++ packages/create-bubbles/template-vp-monorepo-react-nestjs/apps/web/src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import { DatePicker } from 'antd'
+
+const delay = new Promise<void>(resolve => setTimeout(resolve, 2000))
+let resolved = false
+delay.then(() => (resolved = true))
+
+const Home = () => {
+  if (!resolved) throw delay
+
+  return (
+    <div className="w-full bg-red-900">
+      <DatePicker />
+    </div>
+  )
+}
+
+export default Home
