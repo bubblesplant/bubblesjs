e7900167560e56a8a0b926a43cba67a3e891deda
malei <435012357@qq.com>
2026-06-09 12:41:44 +0800

    chore: 更新 .gitignore，重构 utils 函数，更新 tsconfig 配置，添加 tsconfig.json

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,3 +1,9 @@
+import {
+  deepMergeObject,
+  isPlainObject,
+  isReadableStream,
+  tryParseJsonString,
+} from '@bubblesjs/utils'
 import type {
   AlovaGlobalCacheAdapter,
   AlovaOptions,
@@ -9,7 +15,6 @@ import type {
 import { createAlova } from 'alova'
 import type { FetchRequestInit } from 'alova/fetch'
 import adapterFetch from 'alova/fetch'
-import { deepMergeObject, isPlainObject, isReadableStream, tryParseJsonString } from './utils'
 
 type MaybePromise<T> = T | Promise<T>
 type HeaderValue =
