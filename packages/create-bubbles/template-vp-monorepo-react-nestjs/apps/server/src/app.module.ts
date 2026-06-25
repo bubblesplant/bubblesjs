dc17109181666480f167714986864bfd54d4eabd
malei <435012357@qq.com>
2026-06-25 18:37:04 +0800

    chore(template-vp-monorepo-react-nestjs): 完善模板工程配置与切换到fastify

--- packages/create-bubbles/template-vp-monorepo-react-nestjs/apps/server/src/app.module.ts
+++ packages/create-bubbles/template-vp-monorepo-react-nestjs/apps/server/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { appConfig, databaseConfig, llmConfig } from '@/config'
+import { ResponseInterceptor } from '@/common/interceptors/transform'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 const nodeEnv = process.env.NODE_ENV ?? 'development'
 
@@ -15,6 +17,12 @@ const nodeEnv = process.env.NODE_ENV ?? 'development'
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- packages/create-bubbles/template-vp-monorepo-react-nestjs/apps/server/src/main.ts
+++ packages/create-bubbles/template-vp-monorepo-react-nestjs/apps/server/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app.module'
 import { logNetworkUrls } from '@/utils/server-address'
+import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
   const port = Number.parseInt(process.env.PORT ?? '3000', 10)
   const host = process.env.HOST ?? '0.0.0.0'
